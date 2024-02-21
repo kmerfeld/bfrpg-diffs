fcf62ef6d1ce35b9a6288a2752b777f2ca4b8f6f
kyle merfeld <kmerfeld1@gmail.com>
2024-02-21 15:19:28 +0000

    using pandoc to convert to markdown over odt2txt

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 zip --delete Basic-Fantasy-RPG-Rules.odt "Pictures/*"
+pandoc --from=odt Basic-Fantasy-RPG-Rules.odt -o Basic-Fantasy-RPG-Rules.md
 libreoffice --headless --convert-to fodt Basic-Fantasy-RPG-Rules.odt
-odt2txt Basic-Fantasy-RPG-Rules.odt > Basic-Fantasy-RPG-Rules.txt
 rm Basic-Fantasy-RPG-Rules.odt
-git add Basic-Fantasy-RPG-Rules.txt Basic-Fantasy-RPG-Rules.fodt
+git add Basic-Fantasy-RPG-Rules.md Basic-Fantasy-RPG-Rules.fodt
 git commit
